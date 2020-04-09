54b231a54e9da6712f27ad6f665b5a0a1ee061dd
Johan Smarius <jsmarius@hotmail.com>
2020-04-09 14:40:35 +0200

    Created small example

---
+++ FiddlerTest/APIClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Common;
+
+namespace APIClient
+{
+    class Program
+    {
+        private const string RequestUri = "http://localhost:5000/weatherforecast";
+
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var result = await client.GetAsync(RequestUri);
+
+
+            var newForeCast = new WeatherForecast
+            {
+                Date = DateTime.Now.AddMinutes(-5),
+                Summary = "Some summary",
+                TemperatureC = 24
+            };
+
+            var request = JsonSerializer.Serialize(newForeCast);
+
+            var response = await client.PostAsync(RequestUri,
+                new StringContent(request, Encoding.UTF8, "application/json"));
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
